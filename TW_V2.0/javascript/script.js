acc6af40152515ad9795dd2fd6be979b21cd4f18
Adrian <blb.adrian98@gmail.com>
2023-05-07 21:51:07 +0300

    Added mainPage for TW2.0

---
+++ TW_V2.0/javascript/script.js
@@ -0,0 +1,12 @@
+// get the dropdownButton and dropdown elements
+const dropdownButton = document.querySelector('.dropdownButton');
+const dropdown = document.querySelector('.dropdownMenu');
+
+// hide the dropdown menu initially
+dropdown.style.display = 'none';
+
+// add a click event listener to the dropdownButton
+dropdownButton.addEventListener('click', () => {
+  // toggle the display property of the dropdown element
+  dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+});
