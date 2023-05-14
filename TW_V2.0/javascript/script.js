fa910b48ae9edef866af45783ae251e863f48918
Adrian <blb.adrian98@gmail.com>
2023-05-14 19:24:37 +0300

    Filter menu added

--- TW_V2.0/javascript/script.js
+++ TW_V2.0/javascript/script.js
@@ -4,10 +4,13 @@ const dropdownButton = document.querySelector('.dropdownButton');
 const dropdown = document.querySelector('.dropdownMenu');
 const searchBar = document.querySelector('.searchBarContainer');
 const searchButton = document.querySelector('.searchButtonContainer');
+const filterButton = document.querySelector('.filterButtonContainer');
+const filterMenu = document.querySelector('.filterMenuContainer');
 
 // hide the dropdown menu initially
 dropdown.style.display = 'none';
 searchBar.style.display = 'none';
+filterMenu.style.display = 'none';
 
 // add a click event listener to the dropdownButton
 dropdownButton.addEventListener('click', () => {
@@ -18,26 +21,48 @@ dropdownButton.addEventListener('click', () => {
   }
 });
 
-// add a click event listener to the document object
-document.addEventListener('click', (event) => {
-  // close the dropdown menu if the user clicks anywhere else on the page
-  if (!dropdownButton.contains(event.target) && !dropdown.contains(event.target)) {
-    dropdown.style.display = 'none';
-  }
-});
-
 // add a click event listener to the search button
 searchButton.addEventListener('click', () => {
   // toggle the display property of the search button element
   searchBar.style.display = searchBar.style.display === 'none' ? 'flex' : 'none';
 });
 
+filterButton.addEventListener('click', () => {
+  // toggle the display property of the filter button element
+  filterMenu.style.display = filterMenu.style.display === 'none' ? 'flex' : 'none';
+});
+
 // add a click event listener to the document object
 document.addEventListener('click', (event) => {
+  // close the dropdown menu if the user clicks anywhere else on the page
+  if (!dropdownButton.contains(event.target) && !dropdown.contains(event.target)) {
+    dropdown.style.display = 'none';
+  }
   // close the search bar if the user clicks anywhere else on the page
   if (!searchButton.contains(event.target) && !searchBar.contains(event.target)) {
     searchBar.style.display = 'none';
   }
+  // close the filter menu if the user clicks anywhere else on the page
+  if (!filterButton.contains(event.target) && !filterMenu.contains(event.target)) {
+    filterMenu.style.display = 'none';
+  }
+});
+
+// Highlight filter menu list elements when clicked
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach(item => {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('selected')) {
+      item.classList.remove('selected');
+      item.style.backgroundColor = 'white';
+      item.style.color = '#828282';
+    } else {
+      item.classList.add('selected');
+      item.style.backgroundColor = '#7B886B';
+      item.style.color = 'white';
+    }
+  });
 });
 
 const albumsButton = document.querySelector('.albumsButtonContainer');
